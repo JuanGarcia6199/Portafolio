efdbb350e47e6b8bb59d967c83661b0c53731f04
TanjiTatsu <154560844+TanjiTatsu@users.noreply.github.com>
2024-01-12 17:38:55 -0600

    contact form working
    the constact form works!

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ navBtn.addEventListener('click', function () {
   mobileNav.classList.toggle('nav-open');
 });
 
-///////// CONTACT MODAL///////
+///////// CONTACT MODAL ///////
 
 const overlay = document.querySelector('.overlay');
 const contactForm = document.querySelector('.contact-form');
@@ -60,22 +60,55 @@ const contactBtns = document.querySelectorAll('.contactBtn');
 const btnCloseContactForm = document.querySelector('.close-modal');
 const btnOpenContactForm = document.querySelector('.contactBtn');
 
-const OpenContactForm = function () {
-  contactForm.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+const displayContactForm = function () {
+  contactForm.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
 };
 
-const CloseContactForm = function () {
-  contactForm.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
 for (let i = 0; i < contactBtns.length; ++i)
-  contactBtns[i].addEventListener('click', OpenContactForm);
-btnCloseContactForm.addEventListener('click', CloseContactForm);
-overlay.addEventListener('click', CloseContactForm);
+  contactBtns[i].addEventListener('click', displayContactForm);
+btnCloseContactForm.addEventListener('click', displayContactForm);
+overlay.addEventListener('click', displayContactForm);
 
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !contactForm.classList.contains('hidden')) {
-    CloseContactForm();
+    displayContactForm();
   }
+});
+
+///////// SEND EMAIL ///////
+const form = document.querySelector('form');
+const fullName = document.getElementById('name');
+const email = document.getElementById('email');
+const mess = document.getElementById('message');
+
+const sendEmail = function () {
+  const bodymessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Message ${mess.value}`;
+
+  Email.send({
+    Host: 'smtp.elasticemail.com',
+    Username: 'juanfg08.jg@gmail.com',
+    Password: '3628CF2B8DCC12C641EBA7A296B9A298BF98',
+    To: 'juanfg08.jg@gmail.com',
+    From: 'juanfg08.jg@gmail.com',
+    Subject: 'New Message',
+    Body: bodymessage,
+  }).then(message => {
+    if (message === 'OK') {
+      Swal.fire({
+        html: '<h2 class="title-popup"> Message sent successfully! </h2>',
+        title: 'Success!',
+        text: 'Message sent successfully!',
+        icon: 'success',
+        width: '30%',
+        padding: '4rem 0',
+        background: '#fff',
+      });
+    }
+  });
+};
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  sendEmail();
 });
